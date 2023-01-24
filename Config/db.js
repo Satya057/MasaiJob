14d56b6a0e92a7831b37d7d059b651dbb57dc58a
Satya057 <singhsatyaprakash057@gmail.com>
2023-01-24 14:20:21 +0530

    backenddone

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,16 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
-const connection = mongoose.connect(process.env.MONGO_URL)
+// const mongoose = require("mongoose")
+// require("dotenv").config()
+// const connection = mongoose.connect(process.env.MONGO_URL)
 
-module.exports = {
-    connection
-}
+// module.exports = {
+//     connection
+// }
+
+
+
+const mongoose = require("mongoose");
+const connection = () => {
+  return mongoose.connect("mongodb+srv://mock13:mock13@cluster0.uyiueap.mongodb.net/test");
+};
+
+module.exports = connection;
